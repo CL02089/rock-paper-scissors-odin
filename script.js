4df26cae2ff62da9fcbf5ef6a2aa764ba4ead1fc
CL02089 <csaba.lukacs@gmail.com>
2023-10-05 22:04:12 +0200

    Try some logic in functions

--- script.js
+++ script.js
@@ -8,38 +8,58 @@ const getComputerChoice = function () {
 };
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() === computerSelection)
-    return `It's a tie. You've both selected ${playerSelection}`;
-  if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper")
-    return `You lose! Paper beats rock!`;
-  if (
-    playerSelection.toLowerCase() === "rock" &&
-    computerSelection === "scissors"
-  )
+  const playerSelect = playerSelection.toLowerCase();
+  let winner = "";
+  //Rules:
+  // Paper beats rock
+  // Rock beats scissors
+  // Scissors beats paper
+
+  // Here some logic if playerSelect > computerSelection --> player win
+  // if playerSelect < computerSelection -- > computer win
+  // else tie
+  if (playerSelect === computerSelection)
+    return `It's a tie. You've both selected ${playerSelect}`;
+  if (playerSelect === "rock" && computerSelection === "paper") {
+    winner = "computer";
+    return `You lost! Paper beats rock!`;
+  }
+  if (playerSelect === "rock" && computerSelection === "scissors") {
+    winner = "player";
     return `You win! Rock beats scissors!`;
+  }
 
-  if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")
+  if (playerSelect === "paper" && computerSelection === "rock") {
+    winner = "player";
     return `You win! Paper beats rock!`;
+  }
 
-  if (
-    playerSelection.toLowerCase() === "paper" &&
-    computerSelection === "scissors"
-  )
+  if (playerSelect === "paper" && computerSelection === "scissors") {
+    winner = "computer";
     return `You lost! Scissors beats paper!`;
+  }
 
-  if (
-    playerSelection.toLowerCase() === "scissors" &&
-    computerSelection === "rock"
-  )
+  if (playerSelect === "scissors" && computerSelection === "rock") {
+    winner = "computer";
     return `You lost! Rock beats scissors!`;
+  }
 
-  if (
-    playerSelection.toLowerCase() === "scissors" &&
-    computerSelection === "paper"
-  )
+  if (playerSelect === "scissors" && computerSelection === "paper") {
+    winner = "player";
     return `You win! Scissors beats paper!`;
+  }
+
+  //"Return" here with some logic
 }
 
-const playerSelection = "rock";
+const playerSelection = getComputerChoice();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+const game = function () {};
+
+// Call playround
+// Player selection must be prompted
+// log win or lose
+// keeps score
+// diplay the winner
